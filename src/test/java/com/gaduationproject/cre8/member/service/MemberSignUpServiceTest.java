67d09b34e02a60957094fed22b1854cbf3088cb4
Jinu Yi <dionisos198@naver.com>
2024-05-27 21:21:37 +0900

    Test: create logic memberSignUp using TDD (#17)

---
+++ src/test/java/com/gaduationproject/cre8/member/service/MemberSignUpServiceTest.java
@@ -0,0 +1,139 @@
+package com.gaduationproject.cre8.member.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.gaduationproject.cre8.member.entity.Member;
+import com.gaduationproject.cre8.member.repository.MemberRepository;
+import com.gaduationproject.cre8.member.type.Sex;
+import java.time.LocalDate;
+import java.util.Optional;
+import org.aspectj.util.Reflection;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MemberSignUpServiceTest {
+
+    @InjectMocks
+    MemberSignUpService memberSignUpService;
+
+    @Mock
+    MemberRepository memberRepository;
+
+
+    private Member getMember(){
+        Member member = Member.builder()
+                .name("이진우")
+                .email("dionisos198@naver.com")
+                .password("password")
+                .sex(Sex.M)
+                .nickName("dionisos198")
+                .birthDay(LocalDate.of(2023,1,1))
+                .build();
+
+        ReflectionTestUtils.setField(member,"id",1);
+
+        return member;
+    }
+
+    @Test
+    @DisplayName("회원 저장 로직- 정상 상황")
+    public void 회원_저장(){
+
+        //givne
+        Member member = getMember();
+
+
+        //when
+        when(memberRepository.save(any(Member.class))).thenReturn(member);
+        MemberSignUpRequestDto memberSignUpRequestDto = MemberSignUpRequestDto
+                .builder()
+                        .name("이진우")
+                                .email("dionisos198@naver.com")
+                                        .password("password")
+                                                .sex(Sex.M)
+                                                        .nickName("dionisos198")
+                                                                .birthDay(LocalDate.of(2023,1,1))
+                                                                        .build();
+
+
+        //then
+        Long findMemberId = memberSignUpService.saveMember(memberSignUpRequestDto);
+
+        //then
+        assertThat(findMemberId).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("회원 저장 로직- 이메일이 중복되었을 때 ")
+    public void 회원_저장_이메일_중복_예외(){
+
+        //given
+        Member member = getMember();
+
+        //when
+        when(memberRepository.findByEmail(eq(member.getEmail()))).thenReturn(Optional.of(member));
+        MemberSignUpRequestDto memberSignUpRequestDto = MemberSignUpRequestDto
+                .builder()
+                .name("이진우1")
+                .email("dionisos198@naver.com")
+                .password("password1")
+                .sex(Sex.M)
+                .nickName("dionisos1981")
+                .birthDay(LocalDate.of(2023,1,2))
+                .build();
+
+
+        //then
+
+        Exception e = org.junit.jupiter.api.Assertions.assertThrows(EmailDuplicateException.class,()->{
+            memberSignUpService.saveMember(memberSignUpRequestDto)
+        });
+
+        assertEquals("이메일이 중복되었습니다", e.getMessage());
+
+    }
+
+    @Test
+    @DisplayName("회원 저장 로직- 닉네임이 중복되었을 때 ")
+    public void 회원_저장_닉네임_중복_예외(){
+        //given
+        Member member = getMember();
+
+        //when
+        when(memberRepository.findByNickName(eq(member.getNickName()))).thenReturn(Optional.of(member));
+        MemberSignUpRequestDto memberSignUpRequestDto = MemberSignUpRequestDto
+                .builder()
+                .name("이진우1")
+                .email("dionisos1981@naver.com")
+                .password("password1")
+                .sex(Sex.M)
+                .nickName("dionisos198")
+                .birthDay(LocalDate.of(2023,1,2))
+                .build();
+
+        //then
+        Exception e = org.junit.jupiter.api.Assertions.assertThrows(NickNameDuplicateException.class,()->{
+            memberSignUpService.saveMember(memberSignUpRequestDto)
+        });
+
+        assertEquals("닉네임이 중복되었습니다", e.getMessage());
+
+    }
+
+
+
+
+}
