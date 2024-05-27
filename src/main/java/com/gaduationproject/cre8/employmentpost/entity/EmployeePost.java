a49eaae1d902ea41f304122be412aebb99ed678d
Jinu Yi <dionisos198@naver.com>
2024-05-28 00:12:47 +0900

    Fix: wrong on EmployeePostEntity (#17)

--- src/main/java/com/gaduationproject/cre8/employmentpost/entity/EmployeePost.java
+++ src/main/java/com/gaduationproject/cre8/employmentpost/entity/EmployeePost.java
@@ -1,16 +1,17 @@
 package com.gaduationproject.cre8.employmentpost.entity;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("employee_post")
 public class EmployeePost extends EmploymentPost{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
 
     private int careerYear;
 
