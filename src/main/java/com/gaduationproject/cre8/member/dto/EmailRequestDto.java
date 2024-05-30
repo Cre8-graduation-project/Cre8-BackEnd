a20dc0bb15c91afc9162e8ed1ae6b7717cce8ae7
Jinu Yi <dionisos198@naver.com>
2024-05-30 17:39:48 +0900

    Feat: swagger setting on sign up (#19)

--- src/main/java/com/gaduationproject/cre8/member/dto/EmailRequestDto.java
+++ src/main/java/com/gaduationproject/cre8/member/dto/EmailRequestDto.java
@@ -1,5 +1,6 @@
 package com.gaduationproject.cre8.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -10,9 +11,11 @@ import lombok.Setter;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "email 에 대한 인증번호를 전송하고 중복되는 email 이 이미 존재하는지 확인하는 dto")
 public class EmailRequestDto {
 
     @Email(message = "이메일 형식에 맞지 않습니다")
     @NotBlank(message = "이메일을 입력해 주세요")
+    @Schema(description = "사용자가 가입하고자 하는 이메일",example = "blackbox0209@naver.com")
     private String email;
 }
