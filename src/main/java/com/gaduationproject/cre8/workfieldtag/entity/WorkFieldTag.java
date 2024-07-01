2c9abbe8984665b0da2011f5dee7e782316c697e
Jinu Yi <dionisos198@naver.com>
2024-07-02 04:13:47 +0900

    Refactor: change WorkField Entity for flexible (#64)

--- src/main/java/com/gaduationproject/cre8/workfieldtag/entity/WorkFieldTag.java
+++ src/main/java/com/gaduationproject/cre8/workfieldtag/entity/WorkFieldTag.java
@@ -1,17 +1,25 @@
 package com.gaduationproject.cre8.workfieldtag.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Generated;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class WorkFieldTag {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "work_field_tag_id")
@@ -19,6 +27,25 @@ public class WorkFieldTag {
 
     @Column(nullable = false)
     private String name;
+
+    @Builder
+    public WorkFieldTag(String name){
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "workFieldTag",cascade = CascadeType.ALL,orphanRemoval = true)
+    List<WorkFieldSubCategory> workFieldSubCategoryList = new ArrayList<>();
+
+    public void addWorkFieldSubCategory(final String subCategoryName){
+
+        WorkFieldSubCategory workFieldSubCategory = WorkFieldSubCategory.builder()
+                .workFieldTag(this)
+                .name(subCategoryName)
+                .build();
+
+        workFieldSubCategoryList.add(workFieldSubCategory);
+
+    }
 
 
 
