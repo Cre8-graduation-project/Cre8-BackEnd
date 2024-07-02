246a4b82deafdb9d6807dca504ae75561172cd80
Jinu Yi <dionisos198@naver.com>
2024-07-03 02:24:49 +0900

    Feat: make portfolio crud (#63)

---
+++ src/main/java/com/gaduationproject/cre8/portfolio/dto/response/PortfolioSimpleResponseDto.java
@@ -0,0 +1,23 @@
+package com.gaduationproject.cre8.portfolio.dto.response;
+
+import com.gaduationproject.cre8.portfolio.entity.Portfolio;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class PortfolioSimpleResponseDto {
+
+    private Long id;
+    private String accessUrl;
+
+    public static PortfolioSimpleResponseDto from(Portfolio portfolio){
+        return new PortfolioSimpleResponseDto(portfolio.getId(),
+                portfolio.getPortfolioImageList().isEmpty()?null:portfolio.getPortfolioImageList().get(0).getAccessUrl());
+    }
+
+
+}
