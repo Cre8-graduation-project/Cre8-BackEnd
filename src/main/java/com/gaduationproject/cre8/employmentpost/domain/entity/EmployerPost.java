018dcdef0f1cb9c843011532dabc5e66c37e7d8a
Jinu Yi <dionisos198@naver.com>
2024-07-04 01:51:10 +0900

    Feat: EmployerPost CRUD With ErrorCode (#70)

--- src/main/java/com/gaduationproject/cre8/employmentpost/domain/entity/EmployerPost.java
+++ src/main/java/com/gaduationproject/cre8/employmentpost/domain/entity/EmployerPost.java
@@ -1,6 +1,9 @@
 package com.gaduationproject.cre8.employmentpost.domain.entity;
 
 import com.gaduationproject.cre8.employmentpost.domain.type.EnrollDurationType;
+import com.gaduationproject.cre8.employmentpost.domain.type.PaymentMethod;
+import com.gaduationproject.cre8.member.entity.Member;
+import com.gaduationproject.cre8.workfieldtag.entity.WorkFieldTag;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Embedded;
@@ -10,8 +13,12 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,15 +35,42 @@ public class EmployerPost  {
     @Embedded
     private BasicPostContent basicPostContent;
 
-    private int numberOfEmployee;
+    private Integer numberOfEmployee;
 
     @Enumerated(EnumType.STRING)
     private EnrollDurationType enrollDurationType;
 
-    private int minCareerYear;
+    private Integer minCareerYear;
 
     private LocalDate deadLine;
 
+    @OneToMany(mappedBy = "employerPost")
+    List<EmployerPostWorkFieldChildTag> employerPostWorkFieldChildTagList = new ArrayList<>();
 
+    @Builder
+    public EmployerPost(Member member, WorkFieldTag workFieldTag, PaymentMethod paymentMethod, Integer payment, Integer numberOfEmployee,
+             EnrollDurationType enrollDurationType, LocalDate deadLine,Integer minCareerYear) {
 
+        this.basicPostContent = BasicPostContent.builder()
+                .member(member)
+                .workFieldTag(workFieldTag)
+                .paymentMethod(paymentMethod)
+                .payment(payment)
+                .build();
+        this.numberOfEmployee = numberOfEmployee;
+        this.enrollDurationType = enrollDurationType;
+        this.minCareerYear = minCareerYear;
+        this.deadLine = deadLine;
+    }
+
+    public void changeAllExceptMemberAndId(WorkFieldTag workFieldTag,PaymentMethod paymentMethod,Integer payment, Integer numberOfEmployee,
+            EnrollDurationType enrollDurationType,LocalDate deadLine,Integer minCareerYear){
+
+        basicPostContent.changeExceptMember(workFieldTag,paymentMethod,payment);
+        this.numberOfEmployee = numberOfEmployee;
+        this.enrollDurationType = enrollDurationType;
+        this.minCareerYear = minCareerYear;
+        this.deadLine = deadLine;
+
+    }
 }
---
+++ src/main/java/com/gaduationproject/cre8/employmentpost/dto/request/SaveEmployerPostRequestDto.java
@@ -0,0 +1,48 @@
+package com.gaduationproject.cre8.employmentpost.dto.request;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SaveEmployerPostRequestDto {
+
+    private Long workFieldId;
+
+    private List<Long> workFieldChildTagId = new ArrayList<>();
+
+    private String paymentMethod;
+
+    private Integer payment;
+
+    private Integer numberOfEmployee;
+
+    private String enrollDurationType;
+
+    private LocalDate deadLine;
+
+    private Integer minCareerYear;
+
+
+    @Builder
+    public SaveEmployerPostRequestDto(Long workFieldId, List<Long> workFieldChildTagId,
+            String paymentMethod, Integer payment, Integer numberOfEmployee,
+            String enrollDurationType,LocalDate deadLine, Integer minCareerYear) {
+        this.workFieldId = workFieldId;
+        this.workFieldChildTagId = workFieldChildTagId;
+        this.paymentMethod = paymentMethod;
+        this.payment = payment;
+        this.numberOfEmployee = numberOfEmployee;
+        this.enrollDurationType = enrollDurationType;
+        this.deadLine = deadLine;
+        this.minCareerYear = minCareerYear;
+    }
+
+
+
+}
