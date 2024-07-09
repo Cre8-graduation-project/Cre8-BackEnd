a69613ef143ecf9bc5dea85e3b488d491a402911
Jinu Yi <dionisos198@naver.com>
2024-07-10 03:12:17 +0900

    Feat: add intermediate table between emploeePost and childTag (#77)

---
+++ src/main/java/com/gaduationproject/cre8/employmentpost/repository/EmployeePostRepository.java
@@ -0,0 +1,8 @@
+package com.gaduationproject.cre8.employmentpost.repository;
+
+import com.gaduationproject.cre8.employmentpost.domain.entity.EmployeePost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeePostRepository extends JpaRepository<EmployeePost,Long> {
+
+}
---
+++ src/main/java/com/gaduationproject/cre8/employmentpost/repository/EmployeePostWorkFieldChildTagRepository.java
@@ -0,0 +1,9 @@
+package com.gaduationproject.cre8.employmentpost.repository;
+
+
+import com.gaduationproject.cre8.employmentpost.domain.entity.EmployeePostWorkFieldChildTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeePostWorkFieldChildTagRepository extends JpaRepository<EmployeePostWorkFieldChildTag,Long> {
+
+}
