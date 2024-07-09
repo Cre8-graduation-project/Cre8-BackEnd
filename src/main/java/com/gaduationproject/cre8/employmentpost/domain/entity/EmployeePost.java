7222e72fe543a4aa379c7fda58e4a938b47d158a
Jinu Yi <dionisos198@naver.com>
2024-07-10 03:11:24 +0900

    Feat: add column and builder and list on employeePost (#77)

--- src/main/java/com/gaduationproject/cre8/employmentpost/domain/entity/EmployeePost.java
+++ src/main/java/com/gaduationproject/cre8/employmentpost/domain/entity/EmployeePost.java
@@ -1,5 +1,8 @@
 package com.gaduationproject.cre8.employmentpost.domain.entity;
 
+import com.gaduationproject.cre8.employmentpost.domain.type.PaymentMethod;
+import com.gaduationproject.cre8.member.entity.Member;
+import com.gaduationproject.cre8.workfieldtag.entity.WorkFieldTag;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -10,10 +13,13 @@ import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class EmployeePost {
 
     @Id
@@ -24,11 +30,20 @@ public class EmployeePost {
     private BasicPostContent basicPostContent;
     private Integer careerYear;
 
+    @OneToMany(mappedBy = "employeePost")
+    List<EmployeePostWorkFieldChildTag> employeePostWorkFieldChildTagList = new ArrayList<>();
 
+    @Builder
+    public EmployeePost(final Member member,final String title,final WorkFieldTag workFieldTag,final
+            PaymentMethod paymentMethod,final Integer paymentAmount, Integer careerYear) {
 
-
-
-
-
-
+        this.basicPostContent = BasicPostContent.builder()
+                .member(member)
+                .title(title)
+                .workFieldTag(workFieldTag)
+                .paymentMethod(paymentMethod)
+                .paymentAmount(paymentAmount)
+                .build();
+        this.careerYear = careerYear;
+    }
 }
