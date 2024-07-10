1041a3a33ad9ba8cd6f6858b0543e049be514b95
Jinu Yi <dionisos198@naver.com>
2024-07-11 00:58:15 +0900

    Refactor: set BaseEntity on EmployeePost (#81)

--- src/main/java/com/gaduationproject/cre8/employmentpost/domain/entity/EmployeePost.java
+++ src/main/java/com/gaduationproject/cre8/employmentpost/domain/entity/EmployeePost.java
@@ -1,5 +1,6 @@
 package com.gaduationproject.cre8.employmentpost.domain.entity;
 
+import com.gaduationproject.cre8.common.baseentity.BaseEntity;
 import com.gaduationproject.cre8.employmentpost.domain.type.EnrollDurationType;
 import com.gaduationproject.cre8.employmentpost.domain.type.PaymentMethod;
 import com.gaduationproject.cre8.member.entity.Member;
@@ -22,7 +23,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class EmployeePost {
+public class EmployeePost extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
