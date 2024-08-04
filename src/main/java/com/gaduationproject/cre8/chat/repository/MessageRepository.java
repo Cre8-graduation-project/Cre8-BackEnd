5d865feea2ffffa193ca8e36f827f3e93f0dfecd
Jinu Yi <dionisos198@naver.com>
2024-08-04 13:16:41 +0900

    Feat: show chatting room list (#86)

--- src/main/java/com/gaduationproject/cre8/chat/repository/MessageRepository.java
+++ src/main/java/com/gaduationproject/cre8/chat/repository/MessageRepository.java
@@ -3,10 +3,16 @@ package com.gaduationproject.cre8.chat.repository;
 import com.gaduationproject.cre8.chat.entity.ChattingRoom;
 import com.gaduationproject.cre8.chat.entity.Message;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MessageRepository extends JpaRepository<Message,Long> {
 
     List<Message> findByChattingRoom(ChattingRoom chattingRoom);
+
+    @Query("select m from Message m where m.chattingRoom=:chattingRoom order by m.createdAt desc limit 1")
+    Optional<Message> findLatestMessageByChattingRoom(@Param("chattingRoom") ChattingRoom chattingRoom);
 
 }
