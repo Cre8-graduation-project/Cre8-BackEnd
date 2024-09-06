b11a32d2935437269979ca4fb7cba8501d25475c
Jinu Yi <dionisos198@naver.com>
2024-09-06 20:05:16 +0900

    Refactor: deprecated rdbms chat message (#139)

--- src/main/java/com/gaduationproject/cre8/domain/chat/repository/MessageRepository.java
+++ src/main/java/com/gaduationproject/cre8/domain/chat/repository/MessageRepository.java
@@ -1,18 +1,18 @@
-package com.gaduationproject.cre8.domain.chat.repository;
-
-import com.gaduationproject.cre8.domain.chat.entity.ChattingRoom;
-import com.gaduationproject.cre8.domain.chat.entity.Message;
-import java.util.List;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-public interface MessageRepository extends JpaRepository<Message,Long> {
-
-    List<Message> findByChattingRoom(ChattingRoom chattingRoom);
-
-    @Query("select m from Message m where m.chattingRoom=:chattingRoom order by m.createdAt desc limit 1")
-    Optional<Message> findLatestMessageByChattingRoom(@Param("chattingRoom") ChattingRoom chattingRoom);
-
-}
+//package com.gaduationproject.cre8.domain.chat.repository;
+//
+//import com.gaduationproject.cre8.domain.chat.entity.ChattingRoom;
+//import com.gaduationproject.cre8.domain.chat.entity.Message;
+//import java.util.List;
+//import java.util.Optional;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//
+//public interface MessageRepository extends JpaRepository<Message,Long> {
+//
+//    List<Message> findByChattingRoom(ChattingRoom chattingRoom);
+//
+//    @Query("select m from Message m where m.chattingRoom=:chattingRoom order by m.createdAt desc limit 1")
+//    Optional<Message> findLatestMessageByChattingRoom(@Param("chattingRoom") ChattingRoom chattingRoom);
+//
+//}
