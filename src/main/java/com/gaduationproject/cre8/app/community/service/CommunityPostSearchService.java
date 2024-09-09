1deb5223d73f52a82b6b42eb77c79a9a08fc72d2
Jinu Yi <dionisos198@naver.com>
2024-09-09 20:11:51 +0900

    Feat: count query on search post (#143)

--- src/main/java/com/gaduationproject/cre8/app/community/service/CommunityPostSearchService.java
+++ src/main/java/com/gaduationproject/cre8/app/community/service/CommunityPostSearchService.java
@@ -7,6 +7,7 @@ import com.gaduationproject.cre8.app.employmentpost.dto.response.EmployeePostSea
 import com.gaduationproject.cre8.domain.community.dto.CommunityPostSearchDBResponseDto;
 import com.gaduationproject.cre8.domain.community.entity.CommunityPost;
 import com.gaduationproject.cre8.domain.community.repository.CommunityPostRepository;
+import com.gaduationproject.cre8.domain.community.repository.ReplyRepository;
 import com.gaduationproject.cre8.domain.employmentpost.dto.EmployeePostKeyWordSearchDBResponseDto;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,20 +25,21 @@ public class CommunityPostSearchService {
 
 
     private final CommunityPostRepository communityPostRepository;
+    private final ReplyRepository replyRepository;
     public CommunityPostSearchWithSliceResponseDto searchCommunityPostByCommunityBoardId(final Long communityBoardId,
                                                                                          final Pageable pageable){
 
         Slice<CommunityPostSearchDBResponseDto> communityPosts =
                 communityPostRepository.findCommunityPostKeyWordSearchDBByCommunityBoardId(communityBoardId,pageable);
 
+
         List<CommunityPostSearchResponseDto> communityPostSearchResponseDtoList =
                 communityPosts.stream().map(communityPostSearchDBResponseDto -> {
                     return  CommunityPostSearchResponseDto.of(communityPostSearchDBResponseDto.getCommunityPostId(),
                             communityPostSearchDBResponseDto.getTitle(),
-                            0,
+                            replyRepository.totalReplyCount(communityPostSearchDBResponseDto.getCommunityPostId()),
                             communityPostSearchDBResponseDto.getWriterNickName(),
-                            communityPostSearchDBResponseDto.getCreatedAt(),
-                            0);
+                            communityPostSearchDBResponseDto.getCreatedAt());
                 }).collect(Collectors.toList());
 
         return CommunityPostSearchWithSliceResponseDto.of(communityPostSearchResponseDtoList,communityPosts.hasNext());
--- src/main/java/com/gaduationproject/cre8/domain/community/repository/ReplyRepository.java
+++ src/main/java/com/gaduationproject/cre8/domain/community/repository/ReplyRepository.java
@@ -1,5 +1,6 @@
 package com.gaduationproject.cre8.domain.community.repository;
 
+import com.gaduationproject.cre8.domain.community.entity.CommunityPost;
 import com.gaduationproject.cre8.domain.community.entity.Reply;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,6 +17,9 @@ public interface ReplyRepository extends JpaRepository<Reply,Long> {
 
     @Query("select r from Reply r join fetch r.writer where r.parentReply.id=:parentId order by r.createdAt")
     List<Reply> findChildByParentIdOrderByCreatedAtWithFetchWriter(@Param("parentId") Long parentId);
+
+    @Query("select count(r) from Reply r where r.communityPost.id=:communityPostId")
+    int totalReplyCount(@Param("communityPostId")final Long communityPostId);
 
 
 }
