9919610ca9fbf2817778eb101acb9705070ea732
Jinu Yi <dionisos198@naver.com>
2024-10-01 16:12:38 +0900

    Feat: change heart beat rate on connection (#157)

--- src/main/java/com/gaduationproject/cre8/app/chat/config/StompConfig.java
+++ src/main/java/com/gaduationproject/cre8/app/chat/config/StompConfig.java
@@ -3,9 +3,14 @@ package com.gaduationproject.cre8.app.chat.config;
 import com.gaduationproject.cre8.app.chat.handler.StompErrorHandler;
 import com.gaduationproject.cre8.app.chat.handler.StompPreHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -15,8 +20,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class StompConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final StompPreHandler stompPreHandler;
     private final StompErrorHandler stompErrorHandler;
+    private final StompPreHandler stompPreHandler;
+
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -30,11 +36,20 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        ThreadPoolTaskScheduler te = new ThreadPoolTaskScheduler();
+        te.setPoolSize(1);
+        te.setThreadNamePrefix("wss-heartbeat-thread-");
+        te.initialize();
+
         // 메시지를 구독하는 요청 url => 즉 메시지 받을 때
-        registry.enableSimpleBroker("/sub","/queue");
+        registry.enableSimpleBroker("/sub","/queue")
+                .setTaskScheduler(te)
+                .setHeartbeatValue(new long[]{20000,2000});
 
         // 메시지를 발행하는 요청 url => 즉 메시지 보낼 때
         registry.setApplicationDestinationPrefixes("/pub");
+
     }
 
     @Override
