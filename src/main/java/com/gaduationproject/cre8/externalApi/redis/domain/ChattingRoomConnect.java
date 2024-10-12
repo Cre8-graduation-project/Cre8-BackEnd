82f14a0fe9f6e9484993bd2d973a29c4c7a260a3
Jinu Yi <dionisos198@naver.com>
2024-10-12 15:30:41 +0900

    Refactor: check readCount by USER (#191)

--- src/main/java/com/gaduationproject/cre8/externalApi/redis/domain/ChattingRoomConnect.java
+++ src/main/java/com/gaduationproject/cre8/externalApi/redis/domain/ChattingRoomConnect.java
@@ -1,8 +1,10 @@
 package com.gaduationproject.cre8.externalApi.redis.domain;
 
 import jakarta.persistence.Id;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -13,6 +15,7 @@ import org.springframework.data.redis.core.index.Indexed;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@EqualsAndHashCode(exclude = {"sessionId","id"})
 @RedisHash(value = "ChattingRoomConnect")
 public class ChattingRoomConnect {
 
@@ -25,9 +28,14 @@ public class ChattingRoomConnect {
     @Indexed
     private String loginId;
 
+    @Indexed
+    private String sessionId;
+
     @Builder
-    public ChattingRoomConnect(Long chattingRoomId, String loginId) {
+    public ChattingRoomConnect(Long chattingRoomId, String loginId, String sessionId) {
         this.chattingRoomId = chattingRoomId;
         this.loginId = loginId;
+        this.sessionId = sessionId;
     }
+
 }
--- src/main/java/com/gaduationproject/cre8/externalApi/redis/repository/ChattingRoomConnectRepository.java
+++ src/main/java/com/gaduationproject/cre8/externalApi/redis/repository/ChattingRoomConnectRepository.java
@@ -3,12 +3,15 @@ package com.gaduationproject.cre8.externalApi.redis.repository;
 import com.gaduationproject.cre8.externalApi.redis.domain.ChattingRoomConnect;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ChattingRoomConnectRepository extends CrudRepository<ChattingRoomConnect, String> {
 
-    List<ChattingRoomConnect> findByChattingRoomId(Long chattingRoomId);
+    Set<ChattingRoomConnect> findByChattingRoomId(Long chattingRoomId);
 
     Optional<ChattingRoomConnect> findByChattingRoomIdAndLoginId(final Long chattingRoomId,final String loginId);
+
+    Optional<ChattingRoomConnect> findBySessionId(final String sessionId);
 }
 
