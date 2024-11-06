1168ceabf1f51314357fe238506ae8189a514c50
Jinu Yi <dionisos198@naver.com>
2024-11-06 22:11:21 +0900

    Refactor: use File and Link Both of them (#210)

--- src/main/java/com/gaduationproject/cre8/common/response/error/ErrorCode.java
+++ src/main/java/com/gaduationproject/cre8/common/response/error/ErrorCode.java
@@ -33,6 +33,7 @@ public enum ErrorCode {
     CANT_ACCESS_COMMUNITY_POST("자신의 게시글만 수정,삭제 할 수 있습니다"),
     CANT_ACCESS_REPLY("자신의 댓글만 수정,삭제 할 수 있습니다"),
     CANT_REQUEST_TMP_PW("사용자의 정보를 정확히 입력해주세요"),
+    CANT_ALL_BLANK_FILE_URL("MultiPartFile 과 Link 중 하나는 삽입하셔야합니다"),
 
 
 
